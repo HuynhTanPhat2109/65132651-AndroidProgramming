6d08a522ea810573c85c03b95c62e764ea1b6159
HuynhTanPhat2109 <phathuynhcr091105@gmail.com>
2026-03-19 10:06:52 +0700

    Hoàn thành BaiTH7_ListView1

--- BaiTH7_ListView1/app/src/main/java/ntu/tanphat/baith7_listview1/MainActivity.java
+++ BaiTH7_ListView1/app/src/main/java/ntu/tanphat/baith7_listview1/MainActivity.java
@@ -1,8 +1,11 @@
 package ntu.tanphat.baith7_listview1;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -13,7 +16,7 @@ import androidx.core.view.WindowInsetsCompat;
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
-
+    ArrayList<String> DsTinhThanh = new ArrayList<String>();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -22,7 +25,6 @@ public class MainActivity extends AppCompatActivity {
 
         //Hiển thị dữ liệu lên ListView
         //B1: Cần có dữ liệu
-        ArrayList<String> DsTinhThanh = new ArrayList<String>();
         DsTinhThanh.add("Hà Nội");
         DsTinhThanh.add("Tp. Hồ Chí Minh");
         DsTinhThanh.add("Đồng Nai");
@@ -37,5 +39,15 @@ public class MainActivity extends AppCompatActivity {
         //B3: Gắn vào điều khiển hiển thị
         ListView listViewDSTT = (ListView)findViewById(R.id.lvDanhSachTT);
         listViewDSTT.setAdapter(adapterTT);
+        //Gắn bộ lắng nghe vào
+        listViewDSTT.setOnItemClickListener(BoLangNghevaXL);
     }
+    //Tạo bộ lắng nghe và xử lý sự kiện onItemClick, đặt vào một biến vd: BoLangNghevaXl
+    AdapterView.OnItemClickListener BoLangNghevaXL = new AdapterView.OnItemClickListener() {
+        @Override
+        public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+            String strTenTinhChon = DsTinhThanh.get(position);
+            Toast.makeText(MainActivity.this,strTenTinhChon,Toast.LENGTH_LONG).show();
+        }
+    };
 }
