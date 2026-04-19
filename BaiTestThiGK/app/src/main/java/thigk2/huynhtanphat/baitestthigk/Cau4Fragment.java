b4adf64a0000b17bec065975d76ee7f425c7583e
HuynhTanPhat2109 <phathuynhcr091105@gmail.com>
2026-04-19 20:39:26 +0700

    Lần thứ 4

--- BaiTestThiGK/app/src/main/java/thigk2/huynhtanphat/baitestthigk/Cau4Fragment.java
+++ BaiTestThiGK/app/src/main/java/thigk2/huynhtanphat/baitestthigk/Cau4Fragment.java
@@ -7,6 +7,8 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -14,34 +16,26 @@ import android.view.ViewGroup;
  * create an instance of this fragment.
  */
 public class Cau4Fragment extends Fragment {
-
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
     public Cau4Fragment() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment Cau4Fragment.
-     */
-    // TODO: Rename and change types and number of parameters
+    EditText editTextSo1;
+    EditText editTextSo2;
+    EditText editTextKQ;
+    Button nutCong, nutTru, nutNhan, nutChia;
+    void TimDieuKhien(View view){
+        editTextSo1 = view.findViewById(R.id.edtSo1);
+        editTextSo2 = view.findViewById(R.id.edtSo2);
+        editTextKQ = view.findViewById(R.id.edtKq);
+        nutCong = view.findViewById(R.id.btnCong);
+        nutTru = view.findViewById(R.id.btnTru);
+        nutNhan = view.findViewById(R.id.btnNhan);
+        nutChia = view.findViewById(R.id.btnChia);
+    }
     public static Cau4Fragment newInstance(String param1, String param2) {
         Cau4Fragment fragment = new Cau4Fragment();
         Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
         fragment.setArguments(args);
         return fragment;
     }
@@ -49,16 +43,77 @@ public class Cau4Fragment extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_cau4, container, false);
+        View view = inflater.inflate(R.layout.fragment_cau4, container, false);
+        TimDieuKhien(view);
+        nutCong.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                XuLyCong();
+            }
+        });
+
+        nutTru.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                XuLyTru();
+            }
+        });
+
+        nutNhan.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                XuLyNhan();
+            }
+        });
+
+        nutChia.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                XuLyChia();
+            }
+        });
+        return view;
+    }
+    void XuLyCong(){
+        String soA = editTextSo1.getText().toString();
+        String soB = editTextSo2.getText().toString();
+        float soThu1 = Float.parseFloat(soA);
+        float soThu2 = Float.parseFloat(soB);
+        float tong = soThu1 + soThu2;
+        String chuoiKQ = String.valueOf(tong);
+        editTextKQ.setText(chuoiKQ);
+    }
+    void XuLyTru(){
+        String soA = editTextSo1.getText().toString();
+        String soB = editTextSo2.getText().toString();
+        float soThu1 = Float.parseFloat(soA);
+        float soThu2 = Float.parseFloat(soB);
+        float hieu = soThu1 - soThu2;
+        String chuoiKQ = String.valueOf(hieu);
+        editTextKQ.setText(chuoiKQ);
+    }
+    void XuLyNhan(){
+        String soA = editTextSo1.getText().toString();
+        String soB = editTextSo2.getText().toString();
+        float soThu1 = Float.parseFloat(soA);
+        float soThu2 = Float.parseFloat(soB);
+        float tich = soThu1 * soThu2;
+        String chuoiKQ = String.valueOf(tich);
+        editTextKQ.setText(chuoiKQ);
+    }
+    void XuLyChia(){
+        String soA = editTextSo1.getText().toString();
+        String soB = editTextSo2.getText().toString();
+        float soThu1 = Float.parseFloat(soA);
+        float soThu2 = Float.parseFloat(soB);
+        float thuong = soThu1 / soThu2;
+        String chuoiKQ = String.valueOf(thuong);
+        editTextKQ.setText(chuoiKQ);
     }
 }
